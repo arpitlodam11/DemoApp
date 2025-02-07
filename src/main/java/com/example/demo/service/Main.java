e79d8b4ebd4bf9fbf03db569bd9c9500f8aa4694
arpitlodam11 <arpitlodam11@gmail.com>
2025-02-08 03:22:16 +0530

    pull data from main

--- src/main/java/com/example/demo/service/Main.java
+++ src/main/java/com/example/demo/service/Main.java
@@ -3,7 +3,8 @@ package com.example.demo.service;
 public class Main {
     public static void main(String[] args){
         System.out.println("welcome to git hub");
+
          System.out.println("new added");
-        
+        System.out.println("Thirstd afjahufb");
     }
 }
