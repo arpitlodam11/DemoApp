3e27652adfbd8ad7d835f1003f9b9e9ac9a3ca59
arpitlodam11 <arpitlodam11@gmail.com>
2025-02-08 03:12:58 +0530

    Update Main.java

--- src/main/java/com/example/demo/service/Main.java
+++ src/main/java/com/example/demo/service/Main.java
@@ -3,5 +3,7 @@ package com.example.demo.service;
 public class Main {
     public static void main(String[] args){
         System.out.println("welcome to git hub");
+         System.out.println("new added");
+        
     }
 }
